ecdf9f25c26a7ae921cfdfdf685ffd164e2f01cc
sak192 <67677434+sak192@users.noreply.github.com>
2025-09-03 12:50:33 +0530

    initial commit

---
+++ ICommandInterfaceApp/ViewModel/MainWindowVM.cs
@@ -0,0 +1,97 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ICommandInterfaceApp.ViewModel
+{
+    public class MainWindowVM : ObservableObject
+    {
+        public string _result = "0";
+        
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                if(_result != value)
+                {
+                    _result = value;
+                    //notify everyone / all control binded to this property to show the change
+                    RaisePropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        private string _selectedOperation = "";
+        public double _storedValue = 0;
+
+        public double StoredValue
+        {
+            get => _storedValue;
+            set {
+                _storedValue = value;
+                RaisePropertyChanged(nameof(StoredValue));
+            }
+        }
+        public string SelectedOperation
+        {
+            get => _selectedOperation;
+            set=> _selectedOperation = value;
+        }
+
+      
+        public ICommand BtnCmd { get; private set; }
+
+        public MainWindowVM()
+        {
+            BtnCmd = new RelayCommand(ExecuteBtnCmd, CanExecuteBtnCmd);
+        }
+
+        private bool CanExecuteBtnCmd()
+        {
+            return true;
+        }
+
+        private void ExecuteBtnCmd()
+        {
+            
+                if (string.IsNullOrEmpty(SelectedOperation))
+                {
+                    StoredValue = double.Parse(Result);
+                }
+                else
+                {
+                    switch (SelectedOperation)
+                    {
+                        case "+":
+                            StoredValue += double.Parse(Result);
+                            break;
+                        case "-":
+                            StoredValue -= double.Parse(Result);
+                            break;
+
+                        case "*":
+                            StoredValue *= double.Parse(Result);
+                            break;
+                        case "/":
+                            if (double.Parse(Result) == 0)
+                            {
+                                Result = "Error";
+                                SelectedOperation = "";
+                                return;
+                            }
+                            StoredValue /= double.Parse(Result);
+                            break;
+                    }
+                Result = "";
+                SelectedOperation = ""; //resetting after operation
+                }
+        }
+        
+    }
+}
---
+++ ICommandInterfaceApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ICommandInterfaceApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ICommandInterfaceApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowVM();
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(DataContext is MainWindowVM vm && sender is Button btn && btn.Tag is string op)
+            {
+                vm.SelectedOperation = op;
+            }
+        }
+    }
+}
